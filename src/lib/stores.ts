3fffeed918c308d58b86ae3a5e9e5a661c4d293a
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-03-26 00:14:05 +0100

    add: helper functions on top of menuStore

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,4 +2,32 @@ import { writable } from "svelte/store";
 import type { z } from "zod";
 import type { MenuItemSchema } from "./formSchemas";
 
-export const menuStore = writable<z.infer<MenuItemSchema>[]>();
+function _menuStore() {
+  const { subscribe, set, update } = writable<z.infer<MenuItemSchema>[]>([]);
+
+  return {
+    /**
+     * Method to subscribe to changes in the store.
+     */
+    subscribe,
+    /**
+     * Method to add an item to the store.
+     * @param {z.infer<MenuItemSchema>} item - The item to add.
+     */
+    add: (item: z.infer<MenuItemSchema>) => update((items) => [...items, item]),
+
+    /**
+     * Method to remove an item from the store by name.
+     * @param {string} name - The name of the item to remove.
+     */
+    remove: (name: string) =>
+      update((items) => items.filter((item) => item.name !== name)),
+
+    /**
+     * Clear all items from the store.
+     */
+    clear: () => set([]),
+  };
+}
+
+export const menuStore = _menuStore();
