317ae798d977b3defc92061fcdd35f759addc5dd
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-03-31 00:19:28 +0100

    add: assets folder

---
+++ src/lib/components/assets/index.ts
@@ -0,0 +1,9 @@
+import GoogleLogo from './GoogleLogo.svelte';
+import SignupBg from './SignupBg.svelte';
+import SimpleSpinner from './SimpleSpinner.svelte';
+
+export {
+    GoogleLogo,
+    SignupBg,
+    SimpleSpinner as LoadingSpinner
+}
