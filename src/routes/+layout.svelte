614ae20e00bc1cb7defb4bdd7a29f1d17a6ed4cf
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2024-03-31 23:46:46 +0200

    add: started working on dashboard overhaul
    -Moved old dashboard to "/[restaurant]/dashboard"
    -Main, new, dashboard is located under /dashboard-new
    
    dashboard-new is the currently active (wip) section. [restaurant]/dashboard is the old dashboard (im keeping it just for reference)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,8 @@
   import "../app.pcss";
   import { ModeWatcher } from "mode-watcher";
   import { goto } from "$app/navigation";
-    import { page } from "$app/stores";
-    import { SignupBg } from "$lib/components/assets";
 
   supabase.auth.onAuthStateChange((event, session) => {
-
-    /* console.log("event", event)
-
-    console.log("User ID: ", session?.user.id) */
-
-
     if (event === "SIGNED_OUT") {
       const username = session?.user.user_metadata.username
         
@@ -22,28 +14,12 @@
         : toast.success("Goodbye!");
       goto("/");
     }
-
-    /* if(event === "SIGNED_IN") {
-      toast.success("Welcome back!");
-      goto("/dashboard")
-    } */
-    
   });
 </script>
 
 <Toaster position="bottom-right" />
 <ModeWatcher />
-<main class="w-full lg:grid lg:h-screen lg:grid-cols-2">
-{#key $page.url}
-  <slot />
-{/key}
-<div class="hidden bg-muted lg:block p-5">
-  <div class="flex flex-col justify-center items-center size-full">
-    <SignupBg class="w-3/4"/>
-
-  </div>
-</div>
-</main>
+<slot />
 
 <svelte:head>
   <title>LocalGrip</title>
